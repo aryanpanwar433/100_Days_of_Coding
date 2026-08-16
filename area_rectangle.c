3628a561dfeb17e96317765f63e6a580ec461d74
Aryan Panwar <aryanpanwar433@gmail.com>
2026-08-16 22:27:30 +0530

    Adding rectangle code

---
+++ area_rectangle.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int l=5,b=10; // l and b belongs to length and breath
+    printf("the area of rectangleis %d\n",l*b);
+    printf("the perimeter of rectangle %d\n",l+b);
+    return 0;
+}
