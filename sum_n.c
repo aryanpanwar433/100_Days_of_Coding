9ed8eed284434cd4a65ad84471583e290f922e1d
Aryan Panwar <aryanpanwar433@gmail.com>
2026-08-19 01:06:38 +0530

    19/8/2026

---
+++ sum_n.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int n; //natuaral number=n
+    printf("enter n natural number");
+    scanf("%d",&n);
+    printf("the sum of n natural numberis %d\n",n/2*(n+1));
+    return 0;
+}
