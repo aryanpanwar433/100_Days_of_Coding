5e707f61b0516a5c6fbb47416af84d013c464bba
Aryan Panwar <aryanpanwar433@gmail.com>
2026-09-06 03:11:06 +0530

    2/9/2026

--- problem_21.c
+++ problem_21.c
@@ -4,9 +4,19 @@ int main(){
     int sum = 0;
     printf("Enter number = ");
     scanf("%d",&n);
-    for(i=0;i<n;i++){
+   /*for(i=0;i<n;i++){
         sum += 2*i + 1;
     } 
-     printf("Sum of first n odd numbers %d\n", sum);
+     printf("Sum of first n odd numbers %d\n", sum);*/
+    /** while(i<n){
+        sum +=2*i + 1;
+        i++;
+     }*/
+     do{
+        sum +=2*i + 1;
+        i++; 
+     }
+     while(i<n);
+      printf("Sum of first n odd numbers %d\n", sum);
     return 0;
 }
---
+++ problem_22.c
@@ -0,0 +1,13 @@
+ #include<stdio.h>
+ int main(){
+    int i=0,n;
+    int sum = 0;
+     printf("Enter number = ");
+    scanf("%d",&n);
+    while(i<n){
+        sum +=2*i;
+        i++;
+    }
+    printf("Sum of n first even numbers=%d\n",sum);
+    return 0;
+ }
